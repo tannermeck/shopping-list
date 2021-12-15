ed40f32cf3c9020abe4330ab13ac0b7309e061fe
TANNER MECK <steve.meck246@gmail.com>
2021-12-15 15:12:14 -0800

    styling added to shoppinglist component

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,8 +1,16 @@
-
+import style from './shoppingList.css'
 
 function ShoppingList(){
     return (
-        <h1>Hi from shopping list</h1>
+        <section className={style.shoppingContainer}>
+            <div className={style.inputField}>
+                <h1 className={style.header}>Add to Your Shopping List:</h1>
+                <form className={style.form}>
+                    <input className={style.inputBox} type='text' placeholder='Add Item...'/>
+                    <button className={style.submitButton}>Add New Item</button>
+                </form>
+            </div>
+        </section>
     )
 }
 export default ShoppingList;
